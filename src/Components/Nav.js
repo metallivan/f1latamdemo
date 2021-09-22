bd523ed75cc1dad70191e6fec8cc8196bc3a0409
Iván Hernández <iva.hernandez.con@gmail.com>
2021-09-22 02:40:14 -0300

    Agregando animaciones y nueva fuente al menu de navegación

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -7,13 +7,13 @@ const Nav = () => {
     <div className="f1-nav-container">
         <nav className="f1-nav">
             <ul>
-                <li><a href="#">Noticias</a></li>
-                <li><a href="#">Esports</a></li>
-                <li><a href="#">Comunidad</a></li>
-                <li><a href="#"><img className="imgLogo" src={F1Logo} alt="F1 Logo" /></a></li>
-                <li><a href="#">Gaming</a></li>
-                <li><a href="#">¡Apóyanos!</a></li>
-                <li><a href="#">Contacto</a></li>
+                <li><a href="#"><span className="nav-item">Noticias</span></a></li>
+                <li><a href="#"><span className="nav-item">Esports</span></a></li>
+                <li><a href="#"><span className="nav-item">Comunidad</span></a></li>
+                <li><img className="imgLogo" src={F1Logo} alt="F1 Logo" /></li>
+                <li><a href="#"><span className="nav-item">Gaming</span></a></li>
+                <li><a href="#"><span className="nav-item">¡Apóyanos!</span></a></li>
+                <li><a href="#"><span className="nav-item">Contacto</span></a></li>
             </ul>
         </nav>
     </div>
